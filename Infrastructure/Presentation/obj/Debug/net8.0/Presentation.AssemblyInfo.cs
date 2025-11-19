983532da7eacd4b6d602fb7466663dcce80407c7
youssef_74 <youssefessam1293@gmail.com>
2025-11-19 20:02:59 +0200

    Initialize project with Onion Architecture structure

--- Infrastructure/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Infrastructure/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d539ec0bc474d3d8f30f3fe29c99fed3f2f348")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fc631ec5d6cb911e11c348f184abd44b7b6720")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
